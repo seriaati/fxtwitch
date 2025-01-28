1bb42f3a7f6e6df6af7b6c6264e89a966570ccb6
seria <seria.ati@gmail.com>
2025-01-29 08:57:01 +0900

    fix: Redirect to url when errors

--- fxtwitch/app.py
+++ fxtwitch/app.py
@@ -1,4 +1,5 @@
 from contextlib import asynccontextmanager
+import logging
 from typing import AsyncGenerator
 
 import fastapi
@@ -19,6 +20,7 @@ async def app_lifespan(app: fastapi.FastAPI) -> AsyncGenerator[None, None]:
         await app.state.client.close()
 
 
+logger = logging.getLogger("uvicorn")
 app = fastapi.FastAPI(lifespan=app_lifespan)
 
 
@@ -27,9 +29,7 @@ def index() -> fastapi.responses.RedirectResponse:
     return fastapi.responses.RedirectResponse("https://github.com/seriaati/fxtwitch")
 
 
-@app.get("/{clip_author}/clip/{clip_id}")
-@app.get("/clip/{clip_id}")
-async def clip(clip_id: str) -> fastapi.responses.HTMLResponse:
+async def embed_fixer(clip_id: str) -> fastapi.responses.HTMLResponse:
     clip_info = await fetch_clip_info(app.state.client, clip_id=clip_id)
 
     html = f"""
@@ -61,3 +61,25 @@ async def clip(clip_id: str) -> fastapi.responses.HTMLResponse:
     </html>
     """
     return fastapi.responses.HTMLResponse(html)
+
+
+@app.get("/{clip_author}/clip/{clip_id}")
+async def clip_author_clip_id(
+    clip_author: str, clip_id: str
+) -> fastapi.responses.Response:
+    try:
+        return await embed_fixer(clip_id)
+    except Exception:
+        logger.exception("Failed to fetch clip info")
+        return fastapi.responses.RedirectResponse(
+            f"https://clips.twitch.tv/{clip_author}/clip/{clip_id}"
+        )
+
+
+@app.get("/clip/{clip_id}")
+async def clip_id(clip_id: str) -> fastapi.responses.Response:
+    try:
+        return await embed_fixer(clip_id)
+    except Exception:
+        logger.exception("Failed to fetch clip info")
+        return fastapi.responses.RedirectResponse(f"https://twitch.tv/clip/{clip_id}")
